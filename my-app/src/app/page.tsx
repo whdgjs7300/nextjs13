1e43887517ad0338dac08951600a383e2264a28f
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-12-12 19:04:13 +0900

    12/12 클라이언트, 서버 컴포넌트 동작원리

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,14 @@
+import Counter from '@/components/Counter';
 import Image from 'next/image'
-
+import os from 'os'; // 노드 APIs
 
 export default function Home() {
-
+  console.log(os.hostname());
   return (
+    <>
+      <h1>홈페이지다 !!</h1>
+      <Counter/>
+    </>
     
-    <h1>홈페이지다 !!</h1>
   )
 }
