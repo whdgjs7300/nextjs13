98cbee71cfce1115503a121cad2a77bf21832726
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-12-14 16:01:47 +0900

    12/14 SSG렌더링 구현 (products[slug] - )

--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -2,18 +2,18 @@ import { getProducts } from "@/service/products";
 import Link from "next/link";
 
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
 
     // 서버 파일(데이터 베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
-    const products = getProducts();
+    const products = await getProducts();
     return <div>
         <>
             <h1>제품 소개 페이지</h1>
         </>
             <ul>
                 {
-                    products.map((product, index)=> <li key={index}>
-                        <Link href={`products/${product}`}>{product}</Link>
+                    products.map(({id, name}, index)=> <li key={index}>
+                        <Link href={`products/${id}`}>{name}</Link>
                     </li>)
                 }
             </ul>
--- my-app/src/service/products.ts
+++ my-app/src/service/products.ts
@@ -1,9 +1,23 @@
 // API 파일모음집 !
 
-export function getProducts() {
-    return ['shirt', 'pants', 'skirt', 'shoes', 'dress'];
+import path from "path";
+import { promises as fs} from "fs";
+
+export type Product = {
+    id : string,
+    name : string,
+    price : number;
 }
 
-export function getProduct(id : string) {
-    return 'shirt';
+export async function getProducts() : Promise<Product[]> {
+    // data 경로의 json 파일 불러오기
+    const filePath = path.join(process.cwd(), 'data', 'products.json');
+    const data = await fs.readFile(filePath, 'utf-8');
+
+    return JSON.parse(data);
+}
+
+export async function getProduct(id : string): Promise<Product | undefined> {
+    const products = await getProducts();
+    return products.find(item => item.id === id);
 }
